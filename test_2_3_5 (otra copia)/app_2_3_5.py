9e727166f394a4205b85e4c9d8a62d26cd5e9bac
Anggeehh <apinore0703@g.ducaand.es>
2026-04-21 09:56:34 +0200

    Adelantando el codigo

--- test_2_3_5 (otra copia)/app_2_3_5.py
+++ test_2_3_5 (otra copia)/app_2_3_5.py
@@ -5,17 +5,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/debug")
+@app.route("/")
 def ver_coleccion():
     # 1. Conectamos con el archivo de la base de datos
-    conexion = sqlite3.connect("reto_angel.db")
+    conexion = sqlite3.connect("productos.db")
     
     # 2. Configuramos la conexión para que devuelva diccionarios (más fácil para Jinja2)
     conexion.row_factory = sqlite3.Row
     cursor = conexion.cursor()
     
     # 3. Ejecutamos la consulta SQL
-    cursor.execute("SELECT * FROM logs")
+    cursor.execute("SELECT * FROM productos")
     
     # 4. Guardamos todos los resultados en una variable
     datos = cursor.fetchall()
@@ -24,7 +24,7 @@ def ver_coleccion():
     conexion.close()
     
     # 6. Enviamos los datos reales a la plantilla
-    return render_template("galeria.html", items=datos)
+    return render_template("indexgrupal.HTML", items=datos)
 
 if __name__ == "__main__":
    
